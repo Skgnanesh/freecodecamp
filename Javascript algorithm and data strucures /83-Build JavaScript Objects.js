9f74eab30187179c5891d104a65ba221357b41d4
Skgnanesh <skgnanesh7799@gmail.com>
2022-06-30 20:59:54 +0530

    freecodecamp 83

---
+++ Javascript algorithm and data strucures /83-Build JavaScript Objects.js
@@ -0,0 +1,13 @@
+
+
+// Only change code below this line.
+
+var myDog = {
+    // Only change code below this line.
+    "name": "puffy",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["watson", "kat"]
+    
+    
+  };
