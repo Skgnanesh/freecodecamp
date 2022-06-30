fc46dc24c16a3130f36acb1dbaca40cda29a061c
Skgnanesh <skgnanesh7799@gmail.com>
2022-06-30 21:02:01 +0530

    freecodecamp 94

---
+++ Javascript algorithm and data strucures /94-Accessing Nested Arrays.js
@@ -0,0 +1,23 @@
+
+const myPlants = [
+    { 
+      type: "flowers",
+      list: [
+        "rose",
+        "tulip",
+        "dandelion"
+      ]
+    },
+    {
+      type: "trees",
+      list: [
+        "fir",
+        "pine",
+        "birch"
+      ]
+    }  
+  ];
+  
+  // Only change code below this line
+  
+  const secondTree = myPlants[1].list[1];
