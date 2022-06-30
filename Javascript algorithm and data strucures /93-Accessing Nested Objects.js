957bb7830beabf02275d88c01e4840c564a16c11
Skgnanesh <skgnanesh7799@gmail.com>
2022-06-30 21:01:55 +0530

    freecodecamp 93

---
+++ Javascript algorithm and data strucures /93-Accessing Nested Objects.js
@@ -0,0 +1,15 @@
+// Setup
+const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  var gloveBoxContents = myStorage.car.inside["glove box"]; 
+  
