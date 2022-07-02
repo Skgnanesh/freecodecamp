fad00277e63bdcf27f3a2a833cd77d7f159c79c5
Skgnanesh <skgnanesh7799@gmail.com>
2022-07-02 19:28:39 +0530

    ES6

---
+++ ES6/06-Set Default Parameters for Your Functions.js
@@ -0,0 +1,9 @@
+const increment = ( ()=> {
+    "use strict";
+    return (number, value=1) => {
+      return number + value;
+    };
+  })();
+  console.log(increment(5, 2)); // returns 7
+  console.log(increment(5)); // returns 6
+  //only change code above the line
