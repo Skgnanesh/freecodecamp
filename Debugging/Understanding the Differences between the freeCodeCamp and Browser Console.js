a1444923303c698cabe8a61e3abd3a8a97f31095
Skgnanesh <skgnanesh7799@gmail.com>
2022-07-08 19:20:40 +0530

    Debbuging

---
+++ Debugging/Understanding the Differences between the freeCodeCamp and Browser Console.js
@@ -0,0 +1,8 @@
+// Open your browser console.
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+
+// Use console.log() to print the output variable.
+console.log(output);
+
+// Use console.clear() to clear the browser console.
+console.clear();
